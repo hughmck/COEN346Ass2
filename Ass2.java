e871c08b3eb2b7895e27f0247d60109f44f45c5e
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-11-04 16:24:38 -0400

    adding comments

--- Ass2.java
+++ Ass2.java
@@ -7,13 +7,13 @@ public class Ass2 {
     public static void main(String[] args) {
         BufferedReader reader;
         try {
-            reader = new BufferedReader(new FileReader(
+            reader = new BufferedReader(new FileReader( //taking the input file line by line
                     "input.txt"));
             String line = reader.readLine();
             while (line != null) {
                 int timeQuantum = 0;
                 String UserFirst = "";
-                int numberOfProccessesFirstUser  =0;
+                int numberOfProccessesFirstUser  =0; //initializing all the variables which correspond to each line from the input file
                 int processOneArrivalTime = 0;
                 int processOneExecutionTime =0;
                 int processTwoArrivalTime = 0;
@@ -24,37 +24,38 @@ public class Ass2 {
                 int arrivalTimeUserBProcess = 0;
                 int executionTimeUserBProcess = 0;
 
-                timeQuantum = Integer.parseInt(line);
+                timeQuantum = Integer.parseInt(line); //first line in the input file is the time quantum
                 // read next line
-                line = reader.readLine();
+                line = reader.readLine(); //second line in the input file is the username of the first user
                 UserFirst = line;
                 line = reader.readLine();
-                numberOfProccessesFirstUser = Integer.parseInt(line);
+                numberOfProccessesFirstUser = Integer.parseInt(line); //third line is the number of processes the first user has created
                 line = reader.readLine();
-                processOneArrivalTime = Integer.parseInt(line);
+                processOneArrivalTime = Integer.parseInt(line); //the arrival time of the first process user 1 has created
                 line = reader.readLine();
-                processOneExecutionTime = Integer.parseInt(line);
+                processOneExecutionTime = Integer.parseInt(line); //the next line is the execution time of this process
                 line = reader.readLine();
-                processTwoArrivalTime = Integer.parseInt(line);
+                processTwoArrivalTime = Integer.parseInt(line); //same idea as before, this line is the arrival time of the second process created by the user
                 line = reader.readLine();
-                processTwoExecutionTime = Integer.parseInt(line);
+                processTwoExecutionTime = Integer.parseInt(line); //execution time of second process
                 line = reader.readLine();
-                UserSecond = line;
+                UserSecond = line; //username of second user
                 line = reader.readLine();
-                numberOfProcessesSecondUser = Integer.parseInt(line);
+                numberOfProcessesSecondUser = Integer.parseInt(line); //number of processes the second user creates
                 line = reader.readLine();
-                arrivalTimeUserBProcess = Integer.parseInt(line);
+                arrivalTimeUserBProcess = Integer.parseInt(line); //arrival time of the process user B has created
                 line = reader.readLine();
-                executionTimeUserBProcess = Integer.parseInt(line);
-            }
+                executionTimeUserBProcess = Integer.parseInt(line); //execution time of this process
+            } //ALL the variables have been stored in these names, will need to manipulate these to undergo the fair share process
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        FairShareScheduler fairShareScheduler = new FairShareScheduler();
+        FairShareScheduler fairShareScheduler = new FairShareScheduler(); //calilng the fair share scheduler
 
-        fairShareScheduler.clock();
+        fairShareScheduler.clock(); //calling the clock function in the main code, i dont think well need to call the clock, rather we will
+        // need to call the clock in the StartCPU function, then call it here rather than calling the clock
 
 
 
--- FairShareScheduler.java
+++ FairShareScheduler.java
@@ -2,7 +2,7 @@ public class FairShareScheduler extends Thread {
 
     public void startCPU (int timeQuantum, String UserID1, int NumberOfProcessesForFirstUser, int processOneArrivalTime, int processOneExecTime,
                           int processTwoArrivalTime, int processTwoExecTime, String UserID2, int numberOfProcessesForSecondUser,
-                          int arrivalTimeForUserBProcess, int executionTimeForUserBProcess){
+                          int arrivalTimeForUserBProcess, int executionTimeForUserBProcess){ //creating the fair share process in this function by taking in all these parameters
 
         timeQuantum = 0;
         UserID1 = "";
@@ -38,11 +38,11 @@ public class FairShareScheduler extends Thread {
 
 
     }
-    public void clock(){
+    public void clock(){ //creating a clock function which counts the seconds
         int clock = 0;
         while(true){
             try {
-                Thread.sleep(1000);
+                Thread.sleep(1000); //1000 milliseconds per second, therefore it iterates every second
                 clock++;
                 System.out.println("second");
             } catch (InterruptedException e) {
