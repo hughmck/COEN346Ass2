480be66d4de3826c5d4434493ba4305ad5fca2c8
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-11-09 19:24:59 -0500

    adding skeleton of run method

--- UserProcess.java
+++ UserProcess.java
@@ -12,12 +12,27 @@ public class UserProcess extends Thread {
         this.userName = userName;
         this.readyTime= readyTime;
         this.processingTime=processingTime;
+
     }
 
     public void run(){
         System.out.println("Thread " + this.getId() + " is starting " + arrayToString(array)); //outputing the thread ID and its current segment of the array its assigned to
         int inputLength = array.length;
 
+        UserProcess userProcess = new UserProcess(leftHalf); //need to create an object which user process acts on
+        userProcess.start(); //begin execution of left thread
+        try
+        {
+            userProcess.join(); //trying to merge once both threads are done. Can't merge if one thread is still executing
+            UserProcess(array, x , y); //Merging the two array segments
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+        System.out.println("Thread " + this.getId() + " is finished " + arrayToString(array)); //ouputs that the thread is done and what the worked on segment of the array looks like
+
+}
     }
     public void clock(){ //creating a clock function which counts the seconds
         int clock = 0;
