6fc3988e0654b8a3049e1e44a83c08e0c1c6af7d
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-11-10 11:31:51 -0500

    slightly changing things

--- FairShareScheduler.java
+++ FairShareScheduler.java
@@ -7,9 +7,40 @@ public class FairShareScheduler {
         timeQuantum = 0;
         UserID1 = "";
 
-        
 
     }
+    public void run(){
+        System.out.println("Thread " + this.getId() + " is starting " + arrayToString(array)); //outputing the thread ID and its current segment of the array its assigned to
+        int inputLength = array.length;
+
+        UserProcess userProcess = new UserProcess(leftHalf); //need to create an object which user process acts on
+        userProcess.start(); //begin execution of left thread
+        try
+        {
+            userProcess.join(); //trying to merge once both threads are done. Can't merge if one thread is still executing
+            UserProcess(array, x , y); //Merging the two array segments
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+        System.out.println("Thread " + this.getId() + " is finished " + arrayToString(array)); //ouputs that the thread is done and what the worked on segment of the array looks like
 
 
-}
+
+    public void clock(){ //creating a clock function which counts the seconds
+        int clock = 0;
+        while(true){
+        try {
+        Thread.sleep(1000); //1000 milliseconds per second, therefore it iterates every second
+        clock++;
+        System.out.println("second");
+        } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+        }
+        }
+        }
+
+
+
+        }
--- UserProcess.java
+++ UserProcess.java
@@ -1,50 +1,26 @@
 public class UserProcess extends Thread {
+    int processID;
+
+    boolean processStatus; //has the process started yet? true or false
     String userName;
     int readyTime;
     int processingTime;
+
+
 
     private int[] array;
     public UserProcess(int[] array) {
         this.array = array;
     }
 
-    UserProcess(String userName, int readyTime, int processingTime){
+    UserProcess(String userName, int readyTime, int processingTime, int processID, boolean processStatus){
         this.userName = userName;
         this.readyTime= readyTime;
         this.processingTime=processingTime;
+        this.processID=processID;
+        this.processStatus=processStatus;
 
-    }
 
-    public void run(){
-        System.out.println("Thread " + this.getId() + " is starting " + arrayToString(array)); //outputing the thread ID and its current segment of the array its assigned to
-        int inputLength = array.length;
-
-        UserProcess userProcess = new UserProcess(leftHalf); //need to create an object which user process acts on
-        userProcess.start(); //begin execution of left thread
-        try
-        {
-            userProcess.join(); //trying to merge once both threads are done. Can't merge if one thread is still executing
-            UserProcess(array, x , y); //Merging the two array segments
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-        System.out.println("Thread " + this.getId() + " is finished " + arrayToString(array)); //ouputs that the thread is done and what the worked on segment of the array looks like
-
-}
-    }
-    public void clock(){ //creating a clock function which counts the seconds
-        int clock = 0;
-        while(true){
-            try {
-                Thread.sleep(1000); //1000 milliseconds per second, therefore it iterates every second
-                clock++;
-                System.out.println("second");
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 
 }
